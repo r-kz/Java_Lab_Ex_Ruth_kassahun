d7cdc176ee16fd3217171a9193830abadd5b5b98
Ruthkz <ruthzeleke15@gmail.com>
2026-05-21 00:46:08 +0300

    Improved the minor issue that it was facing

--- poker_game/src/Card.java
+++ poker_game/src/Card.java
@@ -1,7 +1,7 @@
 public class Card {
     private final String suit;
     private final String rank;
-    private final int value; // Numerical value for evaluating hand strength
+    private final int value;
 
     public Card(String suit, String rank, int value) {
         this.suit = suit;
